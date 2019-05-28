6c2a42c4457ce472d096ccb016e9760ec6329795
TP211 <1242410592@qq.com>
2019-05-28 12:52:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 # -*- coding: UTF-8 -*-
  
 from Tkinter import *           # 导入 Tkinter 库
+import time
 root = Tk()                     # 创建窗口对象的背景色
                                 # 创建两个列表
 li     = ['C','python','php','html','SQL','java']
 movie  = ['CSS','jQuery','Bootstrap']
 listb  = Listbox(root)          #  创建两个列表组件
 listb2 = Listbox(root)
+for item in time:
+    print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) # 格式化成2016-03-20 11:45:39形式
+  
 for item in li:                 # 第一个小部件插入数据
     listb.insert(0,item)
  
